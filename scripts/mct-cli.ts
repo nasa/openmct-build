6b0a77239d5a3b0c1297cf1a1b6a37b5d6640425
Andrew Henry <akhenry@gmail.com>
2025-07-30 09:14:38 +1000

    Various fixes

--- scripts/mct-cli.ts
+++ scripts/mct-cli.ts
@@ -1,15 +1,9 @@
 #!/usr/bin/env npx tsx
 
-import path from "path";
-import MctYamlConfigurator from "../src/yaml/MctYamlConfigurator";
-import IndexFileCreator from "../src/html/IndexFileCreator";
-import * as fs from 'fs';
 import { parseArgs } from "util";
 import Api from "../src/api/api";
 
-const INSTANCE_PATH = '../instances';
-const DEAULT_INSTANCE = 'default';
-const DEFAULT_CONFIG_FILE = 'example.yaml';
+const DEFAULT_INSTANCE = 'default';
 
 /**
     mct-cli plugins list
@@ -18,24 +12,13 @@ const DEFAULT_CONFIG_FILE = 'example.yaml';
     mct-cli plugins install @openmct/openmct-yamcs --instance something-other-than-default
  */
 
-function build(instance: string) {
-    const fullInstancePath = path.join(__dirname, INSTANCE_PATH, instance);
-    const configPath = path.join(fullInstancePath, DEFAULT_CONFIG_FILE);
-    const configString = fs.readFileSync(configPath, 'utf-8');
-    const configurator = new MctYamlConfigurator();
-    const config = configurator.loadFromYaml(configString);
-    const indexFileCreator = new IndexFileCreator(config);
-    const indexFile = indexFileCreator.generateDocument();
-    fs.writeFileSync(path.join(fullInstancePath, 'index.html'), indexFile.documentElement.innerHTML);
-}
-
 function main() {
     const api = new Api();
     const args = parseArgs({
         options: {
             instance: {
                 type: 'string',
-                default: DEAULT_INSTANCE,
+                default: DEFAULT_INSTANCE,
             },
         },
         allowPositionals: true
@@ -44,7 +27,6 @@ function main() {
     console.log(`noun: ${noun}, verb: ${verb}, name: ${name}, instance: ${args.values.instance}`);
     const command = api.getCommandForNoun(noun);
     command.execute(verb, name, args.values);
-    build(args.values.instance);
 }
 
 main();
