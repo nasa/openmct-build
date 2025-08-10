3c8d3b6fb903122c81ed0502f298ca1a2141b93a
Andrew Henry <akhenry@gmail.com>
2025-08-10 14:54:58 +1000

    Add API for dealing with npm

---
+++ src/api/npm/NpmPackage.ts
@@ -0,0 +1,51 @@
+import { PackageJSON } from "@npm/types";
+import OpenMctConfiguration from "../../openmct/OpenMctConfiguration";
+import path from "path";
+import * as fs from 'fs';
+import * as child_process from 'child_process';
+
+const PACKAGE_DEFAULTS = {
+    private: true,
+    license: 'UNLICESNSED'
+} as PackageJSON;
+
+export default class NpmPackage {
+    #fullInstancePath: string;
+    #packageJsonObject: PackageJSON | undefined;
+    #config: OpenMctConfiguration;
+
+    constructor({fullInstancePath, config}: {fullInstancePath: string, config: OpenMctConfiguration}) {
+        this.#fullInstancePath = fullInstancePath;
+        this.#config = config;
+        this.#loadOrCreate();
+    }
+    #loadOrCreate() {
+        const packageJsonPath = path.join(this.#fullInstancePath, 'package.json');
+        if (!fs.existsSync(packageJsonPath)) {
+            child_process.spawnSync('npm', ['init', '-y'], { cwd: this.#fullInstancePath });
+        }
+
+        const packageOnDisk = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8')) as PackageJSON;
+        this.#packageJsonObject = this.#applyDefaults(packageOnDisk);
+    }
+    #applyDefaults(packageObject: PackageJSON) {
+        packageObject = Object.assign(packageObject, PACKAGE_DEFAULTS);
+
+        return packageObject;
+    }
+    installPackage(packageName: string){
+        child_process.spawnSync('npm', ['install', '--save-dev', packageName], { cwd: this.#fullInstancePath });
+    }
+    install() {
+        const openMctPackageName = `openmct@${this.#config.getOpenMctVersion()}`;
+        this.installPackage(openMctPackageName);
+
+        child_process.spawnSync('npm', ['install'], { cwd: this.#fullInstancePath });
+    }
+    static getOrCreateNodePackage({fullInstancePath, config}: {fullInstancePath: string, config: OpenMctConfiguration}): NpmPackage {
+        const npmPackage = new NpmPackage({fullInstancePath, config});
+        
+        return npmPackage;
+    }
+}
+    
